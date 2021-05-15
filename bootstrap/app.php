44cdb658b0cc8f756904c34cdbb572f20e0e8dd6
Johnvict <johnvict20@gmail.com>
2021-05-15 23:47:06 +0100

    feat: Configured authentication on config

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -104,6 +104,8 @@ $app->register(App\Providers\AppServiceProvider::class);
 $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
 
+$app->register(Tymon\JWTAuth\Providers\LumenServiceProvider::class);
+
 /*
 |--------------------------------------------------------------------------
 | Load The Application Routes
